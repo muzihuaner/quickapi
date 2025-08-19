f02c2caf98b726a4d54715799217ac1ac1393fa1
muzihuaner <muzihuaner@hotmail.com>
2025-08-19 13:53:56 +0800

    修改CDN

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap 5 CSS -->
     <link href="https://gcore.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://gcore.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <style>
         :root {
             --primary-color: #4361ee;

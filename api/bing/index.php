96ca78d59cc523cdecdb08ea3d9165409a5e614b
muzihuaner <muzihuaner@hotmail.com>
2025-08-19 21:27:56 +0800

    更新

--- api/bing/index.php
+++ api/bing/index.php
@@ -4,25 +4,24 @@
      $counter++;  
      $fp = fopen("counter.dat","w");  
      fwrite($fp, $counter);  
-     fclose($fp); 
- ?>
-<?php
-header('Access-Control-Allow-Origin:*');
- $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
- if ($str === false) {
-  exit('Failed to retrieve data from Bing');
- }
- $xml = simplexml_load_string($str);
- if ($xml && isset($xml->image->url)) {
-  $imgurl = 'http://cn.bing.com' . $xml->image->url;
- }
- if(isset($imgurl) && $imgurl){
-  header('Content-Type: image/JPEG');
-  ob_end_clean();
-  readfile($imgurl);
-  flush(); ob_flush();
-  exit();
- }else{
-  exit('error');
- }
+     fclose($fp);
+     
+     header('Access-Control-Allow-Origin:*');
+     $str = file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+     if ($str === false) {
+      exit('Failed to retrieve data from Bing');
+     }
+     $xml = simplexml_load_string($str);
+     if ($xml && isset($xml->image->url)) {
+      $imgurl = 'http://cn.bing.com' . $xml->image->url;
+     }
+     if(isset($imgurl) && $imgurl){
+      header('Content-Type: image/JPEG');
+      ob_end_clean();
+      readfile($imgurl);
+      flush(); ob_flush();
+      exit();
+     }else{
+      exit('error');
+     }
 ?>
--- api/zhihu/index.php
+++ api/zhihu/index.php
@@ -4,21 +4,8 @@
      $counter++;  
      $fp = fopen("counter.dat","w");  
      fwrite($fp, $counter);  
-     fclose($fp); 
- ?>
-<?php
- header('Content-type:application/json; charset=utf-8');
- $response = file_get_contents('http://news-at.zhihu.com/api/2/news/latest');
- if ($response === false) {
-     http_response_code(500);
-     echo json_encode(['error' => 'Failed to fetch data from Zhihu API']);
- } else {
-     $data = json_decode($response);
-     if (json_last_error() === JSON_ERROR_NONE) {
-         echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-     } else {
-         http_response_code(500);
-         echo json_encode(['error' => 'Invalid JSON response from Zhihu API']);
-     }
- }
+     fclose($fp);
+     
+     header('Content-type:application/json; charset=utf-8');
+     readfile('http://news-at.zhihu.com/api/2/news/latest');
 ?>
